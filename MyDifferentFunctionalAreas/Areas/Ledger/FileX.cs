6ed7f0f5a6be88dd0a09b36dbb3d221c3a017778
Jose Saez <jsaez@foreworth.com>
2021-03-01 10:51:07 +0100

    Moved File X from Billing to Ledger

---
+++ MyDifferentFunctionalAreas/Areas/Ledger/FileX.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDifferentFunctionalAreas.Areas.Billing
+{
+    public class FileX
+    {
+        public FileX()
+        {
+
+        }
+
+        private int  CalculateVeryImportantThings(int x)
+        {
+            return x * 1000;
+        }
+
+    }
+}
