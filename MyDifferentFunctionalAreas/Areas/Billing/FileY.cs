329010b01d831b782ffbbe6731d49cc6f2fc5a85
Jose Saez <jsaez@foreworth.com>
2021-04-01 10:51:07 +0200

    Added file Y

---
+++ MyDifferentFunctionalAreas/Areas/Billing/FileY.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDifferentFunctionalAreas.Areas.Billing
+{
+    public class FileY
+    {
+        public FileY()
+        { 
+        
+        }
+
+        public string GetMyName()
+        {
+            return "Cool guy";
+        }
+    }
+}
