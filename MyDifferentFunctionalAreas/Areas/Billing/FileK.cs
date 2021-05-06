a33b45da62bcf6d29055678363b09ec363596dad
Jose Saez <jsaez@foreworth.com>
2021-05-06 10:51:07 +0200

    Added file K

---
+++ MyDifferentFunctionalAreas/Areas/Billing/FileK.cs
@@ -0,0 +1,11 @@
+﻿namespace MyDifferentFunctionalAreas.Areas.Billing
+{
+    public class FileK
+    {
+
+        public string propertyA { get; set; }
+        public string propertyB { get; set; }
+        public string propertyC { get; set; }
+    }    
+        
+}
