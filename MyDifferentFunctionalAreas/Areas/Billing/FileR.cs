dce17ef0ca8ff020ff516d137dcf6bf6ff51bc88
Jose Saez <jsaez@foreworth.com>
2021-06-07 10:51:07 +0200

    added file R to Billing

---
+++ MyDifferentFunctionalAreas/Areas/Billing/FileR.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDifferentFunctionalAreas.Areas.Billing
+{
+    public class FileR
+    {
+        
+    }
+
+    public class FileRR : FileR
+    {
+        private int Number1;
+        private int Number2;
+        private int Number3;
+    }
+}
